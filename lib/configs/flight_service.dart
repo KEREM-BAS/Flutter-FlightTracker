01eccb60f5f01d3df8813c96060d8a0eb00616f4
KEREM BAS <kerembas2000@gmail.com>
2022-11-01 13:03:18 +0300

    main

--- lib/configs/flight_service.dart
+++ lib/configs/flight_service.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flighttracker/configs/all_state.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class FlightService {
   Dio _dio = Dio();
--- lib/views/map_page.dart
+++ lib/views/map_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, non_constant_identifier_names
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -10,9 +10,24 @@ class MapPage extends StatefulWidget {
   State<MapPage> createState() => _MapPageState();
 }
 
+GoogleMapController? _mapController;
+
 class _MapPageState extends State<MapPage> {
+  LatLng _sw = LatLng(49.674, -14.015517);
+  LatLng _ne = LatLng(61.061, 2.0919117);
+  void _OnMapCreated(GoogleMapController controller) async {
+    _mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
+    setState(() {
+      _mapController?.animateCamera(
+        CameraUpdate.newLatLngBounds(
+            LatLngBounds(southwest: _sw, northeast: _ne), 5),
+      );
+    });
+
     return Scaffold(
       body: Stack(
         children: [
@@ -20,9 +35,10 @@ class _MapPageState extends State<MapPage> {
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
             child: GoogleMap(
+              onMapCreated: _OnMapCreated,
               mapType: MapType.satellite,
               initialCameraPosition: CameraPosition(
-                target: LatLng(41.1082, 28.9784),
+                target: LatLng(_sw.latitude, _ne.longitude),
                 zoom: 3,
               ),
             ),
