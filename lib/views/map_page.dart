4242e48fbcfef937e5101d2c087d1e0b90de0ac7
KEREM BAS <kerembas2000@gmail.com>
2022-11-02 17:22:51 +0300

    asdda

--- lib/views/map_page.dart
+++ lib/views/map_page.dart
@@ -17,7 +17,7 @@ GoogleMapController? _mapController;
 
 class _MapPageState extends State<MapPage> {
   LatLng _sw = LatLng(49.674, -14.11517);
-  LatLng _ne = LatLng(61.061, 2.0919127);
+  LatLng _ne = LatLng(61.061, 2.09349127);
   void _OnMapCreated(GoogleMapController controller) async {
     _mapController = controller;
   }
